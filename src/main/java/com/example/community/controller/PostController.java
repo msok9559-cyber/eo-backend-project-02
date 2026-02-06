2647e9f43d4ee9b45282ca7036c80c46b9a19b21
Kim, Jaeung <kimjw5076@naver.com>
2026-02-06 17:08:28 +0900

    프로젝트 5일차(0206) - 작업

--- src/main/java/com/example/community/controller/PostController.java
+++ src/main/java/com/example/community/controller/PostController.java
@@ -1,6 +1,6 @@
 package com.example.community.controller;
 
-import com.example.community.domain.Criteria;
+import com.example.community.domain.post.Criteria;
 import com.example.community.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,14 +18,4 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Slf4j
 @RequiredArgsConstructor
 public class PostController {
-    private final PostService postService;
-
-    @GetMapping({"", "/list" })
-    public String list(@RequestParam("boardId") Long boardId, Criteria criteria, Model model) {
-        log.info("List boardId = {}, criteria={}", boardId,criteria);
-
-        Pageable pagealbe = PageRequest.of(criteria.getPage() - 1,
-                criteria.getSize(), Sort.by(Sort.Direction.DESC, "fixed")
-                        .and(Sort.by(Sort.Direction.DESC, "id")));
-    }
 }
--- src/test/java/com/example/community/persistence/MessageRepositoryTest.java
+++ src/test/java/com/example/community/persistence/MessageRepositoryTest.java
@@ -12,5 +12,5 @@ class MessageRepositoryTest {
     @Autowired
     private MessageRepository messageRepository;
 
-    private MessageRepository getMessageRepository() {}
+//    private MessageRepository getMessageRepository() {}
 }
